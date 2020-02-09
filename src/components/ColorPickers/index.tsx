0416f35009df54904e44ff682f63c1cf57d77845
xuhongling <583746837@qq.com>
2020-02-09 22:50:40 +0800

    完善功能

--- src/components/ColorPickers/index.tsx
+++ src/components/ColorPickers/index.tsx
@@ -2,28 +2,38 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { RootState } from '@/store'
 import { bindActionCreators } from 'redux'
+import { SketchPicker } from 'react-color'
+import classnames from 'classnames'
 import styles from './style.less'
 
 type Props = ReturnType<typeof bindActionCreators>
+type State = {
+	isShowColorPickers: boolean,
+	background: any
+}
 
 // @ts-ignore: 不可达代码错误。 用装饰器简写方式
 @connect(
   (state: RootState) => ({ globalChart: state.globalChart.globalChart }),
   dispatch => ({})
 )
-export default class ColorPickers extends React.Component<Props> {
+export default class ColorPickers extends React.Component<Props,State> {
 
 	constructor(props:any) {
 		super(props)
-		setTimeout(()=>{
-			console.log(this.props.globalChart)
-		},3000)
+		this.state = {
+			isShowColorPickers: false,
+			background: '#fff'
+		}
 	}
+  handleChangeComplete = (color:any) => {
+    this.setState({ background: color.hex });
+  }
 
 	public render() {
 		return (
-			<div className={styles.colorPickers}>
-				
+			<div className={classnames(styles.colorPickers, {[`${styles.showColorPickers}`]: this.state.isShowColorPickers})}>
+				<SketchPicker color={this.state.background} onChangeComplete={ this.handleChangeComplete } />
 			</div>
 		)
 	}
