9286bfe9be750b5947dc2cb0fe7f539b57523ff9
xuhongling <583746837@qq.com>
2020-02-09 10:25:36 +0800

    完善功能

---
+++ src/components/ColorPickers/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './style.less'
+
+export default class ColorPickers extends React.Component {
+
+	constructor(props:any) {
+		super(props)
+	}
+
+	public render() {
+		return (
+			<div className={styles.colorPickers}>
+				
+			</div>
+		)
+	}
+}
