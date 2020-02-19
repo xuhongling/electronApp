eebf434450575bd86c148e81f29086c7beb8f59c
xuhongling <583746837@qq.com>
2020-02-19 22:19:08 +0800

    选项卡切换

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,35 +1,49 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './style.less'
 
-export default class Sidebar extends React.Component {
+type Props = {}
+type State = {
+	isType: string,
+	sidebarList: any[]
+}
+
+export default class Sidebar extends React.Component<Props,State> {
 
 	constructor(props:any) {
 		super(props)
+		this.state = {
+			isType: 'WholeCar',
+			sidebarList: [
+				{icon: 'iconfont icon-che', name: '整车', type: 'WholeCar'},
+				{icon: 'iconfont icon-dianji', name: '电机', type: 'ElectricMachinery'},
+				{icon: 'iconfont icon-guzhang', name: '故障', type: 'Fault'},
+				{icon: 'iconfont icon-bianyaqi', name: '高压附件', type: 'HighVoltage'},
+				{icon: 'iconfont icon-battery', name: '电池', type: 'Battery'},
+				{icon: 'iconfont icon-kongzi', name: 'RCU', type: 'RCU'},
+				{icon: 'iconfont icon-yaokong', name: '手持遥控', type: 'HandheldControl'},
+				{icon: 'iconfont icon-yibiao', name: 'EBS', type: 'EBS'},
+				{icon: 'iconfont icon-xinxi', name: '惯导信息', type: 'Information'}
+			]
+		}
 	}
-	/*
-	*电机 ElectricMachinery
-	*高压附件  HighVoltage
-	*故障  Fault
-	*整车  WholeCar
-	RCU
-	*电池  Battery
-	手持遥控  HandheldControl
-	*EBS
-	*惯导信息  Information
-	 */
+
+	handleClickList = (item:any)=> {
+		this.setState({isType: item.type})
+	}
 
 	public render() {
 		return (
 			<ul className={styles.sidebar}>
-				<li className={styles.listItem}><i className="iconfont icon-che"></i><span>整车</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-dianji"></i><span>电机</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-guzhang"></i><span>故障</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-bianyaqi"></i><span>高压附件</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-battery"></i><span>电池</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-kongzi"></i><span>RCU</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-yaokong"></i><span>手持遥控</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-yibiao"></i><span>EBS</span></li>
-				<li className={styles.listItem}><i className="iconfont icon-xinxi"></i><span>惯导信息</span></li>
+				{
+					this.state.sidebarList.map((item,index)=>{
+						return(
+							<li className={classnames(styles.listItem, {[`${styles.isActive}`]: this.state.isType===item.type})} onClick={()=>this.handleClickList(item)} key={index}>
+								<i className={item.icon}></i><span>{item.name}</span>
+							</li>
+						)
+					})
+				}
 			</ul>
 		)
 	}
