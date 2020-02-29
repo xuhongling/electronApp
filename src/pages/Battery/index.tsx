2376e16c03629bab28bf04a975f4c9f0abe5626e
xuhongling <583746837@qq.com>
2020-02-29 23:12:03 +0800

    完善功能

--- src/pages/Battery/index.tsx
+++ src/pages/Battery/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import CreateChart from 'components/CreateChart'
+import ColorPickers from 'components/ColorPickers'
+import monitorRule from 'static/monitorRule'
 import styles from './style.less'
 
-export default class Battery extends React.Component {
+type Props = {}
+type State = {
+	ruleType: any[]
+}
+
+export default class Battery extends React.Component<Props,State> {
 
 	constructor(props:any) {
 		super(props)
+		this.state = {
+			ruleType: []
+		}
+	}
+
+	componentDidMount() {
+		let ruleTypeData:any[] = []
+		for (let i = 0; i < monitorRule.length; i++) {
+			if (monitorRule[i].type_name === '电池') {
+				ruleTypeData.push(monitorRule[i])
+			}
+		}
+		console.log(ruleTypeData,'电池')
+		this.setState({ruleType: ruleTypeData})
 	}
 
 	public render() {
 		return (
 			<div className={styles.battery}>
-				battery
+				<CreateChart/>
+				<ColorPickers/>
 			</div>
 		)
 	}
--- src/pages/WholeCar/index.tsx
+++ src/pages/WholeCar/index.tsx
@@ -1,12 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { RootState, actions } from '@/store'
 import CreateChart from 'components/CreateChart'
 import ColorPickers from 'components/ColorPickers'
+import fileData from 'static/fileData'
+import monitorRule from 'static/monitorRule'
 import styles from './style.less'
 
-export default class WholeCar extends React.Component {
+type Props = ReturnType<typeof bindActionCreators>
+type State = {}
+// @ts-ignore: 不可达代码错误。 用装饰器简写方式
+@connect(
+  (state: RootState) => ({
+    fileData: state.fileData.fileData,
+    legendData: state.legendData.legendData,
+  }),
+  dispatch => ({
+    ...bindActionCreators(
+      {
+        setGlobalChart: (globalChart: object) => actions.globalChart.setGlobalChart(globalChart)
+      },
+      dispatch
+    )
+  })
+)
+export default class WholeCar extends React.Component<Props,State> {
 
 	constructor(props:any) {
 		super(props)
+	}
+
+	componentDidMount() {
+		for (let i = 0; i < fileData.length; i++) {
+			let canId:any = fileData[i].ID号
+			for (let j = 0; j < monitorRule.length; j++) {
+				let can_id:any =monitorRule[j].can_id
+				if (can_id === canId) {
+					//console.log(monitorRule[j].name,'sss')
+					if (monitorRule[j].name==='1桥转角') {
+						console.log(parseInt(fileData[i].时间标识),'111')
+					}
+				}
+			}
+		}
 	}
 
 	public render() {
