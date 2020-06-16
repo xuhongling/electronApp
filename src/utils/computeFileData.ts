34ea4c7a2b4763ad893f155a21a091a3cb3cc2db
xuhongling <583746837@qq.com>
2020-06-16 22:05:59 +0800

    完善移位运算计算

--- src/utils/computeFileData.ts
+++ src/utils/computeFileData.ts
@@ -32,17 +32,94 @@ const computeFileData = (fileData:any[])=>{
 	// 解析的数据
 	let columnData:any[] = []
 	for (let i = 0; i < filterColumnData.length; i++) {
+
 		let arrData = filterColumnData[i].DataHEX.replace(/^\s+|\s+$/g,"").split(" ")
 		arrData.push(...['0x'])
 		let baseData = arrData.reverse().join("")
+
 		let formatData
 		for (let j = 0; j < selectAllData.length; j++) {
-			let filterColumnDataCanID = filterColumnData[i].CanID
-			let can_id = selectAllData[j].can_id.toString().toLowerCase()
-			if (filterColumnDataCanID === can_id) {
-				// debugger
-				let computeBaseData = (baseData / Math.pow(2, Number(selectAllData[j].start_bit))) & (Math.pow(2, Number(selectAllData[j].bit_size)) - 1)
-				formatData = computeBaseData * Number(selectAllData[j].scale) + Number(selectAllData[j].value_offset)
+			// let filterColumnDataCanID = filterColumnData[i].CanID
+			// let can_id = selectAllData[j].can_id.toString().toLowerCase()
+			let selectName = filterColumnData[i].selectName
+			let name = selectAllData[j].name
+
+			if (selectName === name) {
+				let start_bit = selectAllData[j].start_bit
+				let bit_size = selectAllData[j].bit_size
+				// invertmode=1的情况
+				if (selectAllData[j].convert_mode === "1") {
+					let high_val:any = baseData.substr(0,10)
+					let low_val:any = `0x${baseData.substr(-8,8)}`
+					let high_part = 0
+					let low_part = 0
+					let computeBaseData
+
+					if (Number(start_bit) + Number(bit_size) <= 32) {
+						if (bit_size === 32) {
+							computeBaseData = (low_val >>> start_bit)
+						} else {
+							computeBaseData = (low_val >>> start_bit) & ((1 << bit_size) - 1)
+						}
+					} else if (Number(start_bit) + Number(bit_size) > 32 && Number(start_bit) < 32) {
+						low_part = (low_val >>> start_bit) & ((1 << (32 - start_bit)) - 1)
+						high_part = high_val & ((1 << (Number(start_bit) + Number(bit_size) - 32)) - 1)
+						computeBaseData = (high_part << (32 - start_bit)) | low_part
+					} else {
+						if (bit_size >= 32) {
+							computeBaseData = (high_val >>> (start_bit - 32))
+						} else {
+							computeBaseData = (high_val >>> (start_bit - 32)) & ((1 << bit_size) - 1)
+						}
+					}
+					formatData = computeBaseData * Number(selectAllData[j].scale) + Number(selectAllData[j].value_offset)
+				}
+
+				// invertmode=2的情况
+				if (selectAllData[j].convert_mode === "2") {
+					let high_val:any = '0x' + baseData.substr(-2,2) + baseData.substr(-4,2) + baseData.substr(-6,2) + baseData.substr(-8,2)
+					let low_val:any = '0x' + baseData.substr(8,2) + baseData.substr(6,2) + baseData.substr(4,2) + baseData.substr(2,2)
+					let high_part = 0
+					let low_part = 0
+					let computeBaseData
+
+					if (Number(start_bit) + Number(bit_size) <= 32) {
+						if (bit_size === 32) {
+							computeBaseData = (high_val >>> (32 - start_bit - bit_size))
+						} else {
+							computeBaseData = (high_val >>> (32 - start_bit - bit_size)) & ((1 << bit_size) - 1)
+						}
+					} else if (Number(start_bit) + Number(bit_size) > 32 && Number(start_bit) < 32) {
+						low_part = (low_val >>> (64 - start_bit - bit_size))
+						high_part = high_val & ((1 << (64 - start_bit - bit_size)) - 1)
+						computeBaseData = (high_part << (Number(start_bit) + Number(bit_size) - 32)) | low_part
+					} else {
+						if (bit_size >= 32) {
+							computeBaseData = (low_val >>> (64 - start_bit - bit_size))
+						} else {
+							computeBaseData = (low_val >>> (64 - start_bit - bit_size)) & ((1 << bit_size) - 1)
+						}
+					}
+					formatData = computeBaseData * Number(selectAllData[j].scale) + Number(selectAllData[j].value_offset)
+				}
+
+				/*let high_val:any = baseData.substr(0,10)
+				let low_val:any = `0x${baseData.substr(-8,8)}`
+				let high_part = 0
+				let low_part = 0
+				let computeBaseData
+				if (start_bit + bit_size <= 32) {
+					computeBaseData = (low_val / Math.pow(2, Number(start_bit))) & (Math.pow(2, Number(bit_size)) - 1)
+				} else if (Number(start_bit) + Number(bit_size) > 32 && start_bit < 32) {
+					low_part = (low_val * Math.pow(2, Number(start_bit))) & (Math.pow(2, Number(32 - start_bit)) - 1)
+					high_part = high_val & (Math.pow(2, Number(start_bit + bit_size - 32)) - 1)
+					computeBaseData = (high_part / Math.pow(2, Number(32 - start_bit)) ) | low_part
+				} else {
+					computeBaseData = (high_val * Math.pow(2, Number(start_bit - 32))) & (Math.pow(2, Number(bit_size)) - 1)
+				}
+
+				//let computeBaseData = (baseData / Math.pow(2, Number(selectAllData[j].start_bit))) & (Math.pow(2, Number(selectAllData[j].bit_size)) - 1)
+				formatData = computeBaseData * Number(selectAllData[j].scale) + Number(selectAllData[j].value_offset)*/
 			}
 		}
 		
